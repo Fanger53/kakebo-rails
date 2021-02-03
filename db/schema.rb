93681c8223728002d48709d4ef6d8618134f4b8f
David Leonardo <fanger53@gmail.com>
2021-02-03 00:08:35 -0500

    creation and configuration of group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_020128) do
+ActiveRecord::Schema.define(version: 2021_02_03_044103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "transfers", force: :cascade do |t|
     t.string "name"
@@ -39,5 +48,6 @@ ActiveRecord::Schema.define(version: 2021_02_03_020128) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "transfers", "users"
 end
