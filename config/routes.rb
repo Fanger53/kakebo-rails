b9eb37994545c0f3581c420bae733562bd095d6c
David Leonardo <fanger53@gmail.com>
2021-02-11 13:53:07 -0500

    new user controller and home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root 'transfers#index'
+  root 'users#home'
+  resources :users
   resources :transfers
   resources :groups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
