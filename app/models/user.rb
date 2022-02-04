e77d9c6d80591b83c9961a0c543fc0b2ba0d7153
fanger53 <fanger53@gmail.com>
2022-02-04 17:27:02 -0500

    add facebook and google auth with username and mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,18 @@ class User < ApplicationRecord
   has_many :transfers
   has_many :groups
   has_one_attached :avatar
-  validates :username, presence: true, length: { in: 3..15 }
-  validates :username, uniqueness: true
-  validates :username, format: { with: /[a-zA-Z0-9]/ }
+  # validates :username, presence: true, length: { in: 3..15 }
+  # validates :username, uniqueness: true
+  # validates :username, format: { with: /[a-zA-Z0-9]/ }
   after_commit :add_default_avatar, on: %i[create update]
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
+      user.username = provider_data.info.first_name
+      # user.avatar = provider_data.info.image
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
+      # user.avatar.attach(io: provider_data.info.image, filename: "user_avatar.jpg ")
     end
   end
 
