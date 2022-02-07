997f646c07ddcfed00b7a8879658e99f29604cba
fanger53 <fanger53@gmail.com>
2022-02-07 15:10:42 -0500

    fix rubocop linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   after_commit :add_default_avatar, on: %i[create update]
 
   def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.username = provider_data.info.first_name
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
