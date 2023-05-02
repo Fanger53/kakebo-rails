6c3571590dd423d7890b9cca07d8bbe2226cc619
Fanger53 <fanger53@gmail.com>
2023-05-02 13:32:52 -0500

    refactoring modal component

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ModalComponent < ViewComponent::Base
-  def initialize(title:, body:)
+  def initialize(title:, body:, id:)
     @title = title
     @body = body
+    @id = id
   end
 end
