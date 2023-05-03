9214812d669e3906e6b93553d3f32b0773bb3cde
Fanger53 <fanger53@gmail.com>
2023-05-03 06:09:44 -0500

    change bootstrap and adding incomes and transfers charts.js

--- app/javascript/packs/incomes_chart_generator.js
+++ app/javascript/packs/incomes_chart_generator.js
@@ -1,14 +1,14 @@
 import Chart from 'chart.js/auto';
 
-const charGenerator = () => { document.addEventListener('turbolinks:load', () => {
-    var ctx = document.getElementById('home__chart').getContext('2d');
+const incomesChartGenerator = () => { document.addEventListener('turbolinks:load', () => {
+    var ctx = document.getElementById('incomes_home__chart').getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'bar',
     options: {
       plugins: {
         title: {
           display: true,
-          text: 'Custom Chart Title',
+          text: 'Total Incomes',
           padding: {
               top: 10,
               bottom: 30
@@ -20,11 +20,11 @@ const charGenerator = () => { document.addEventListener('turbolinks:load', () =>
       labels: JSON.parse(ctx.canvas.dataset.labels),
       datasets: [{
         data: JSON.parse(ctx.canvas.dataset.data),
-        label: "Transfers",
+        label: "Incomes",
       }]
     },
     });
   })
 }
 
-export default charGenerator;
+export default incomesChartGenerator;
