bf8fa3c7b5ef72c4de30730059d8b688fec20942
Fanger53 <fanger53@gmail.com>
2023-05-03 06:11:37 -0500

    change bootstrap and adding incomes and transfers charts.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@popperjs/core"
-import "../../assets/stylesheets/application"
-import "bootstrap/dist/css/bootstrap"
-import "bootstrap/dist/js/bootstrap"
+import 'bootstrap';
 import "controllers"
-import charGenerator from "./chart_generator"
+import transfersChartGenerator from "./transfers_chart_generator"
+import incomesChartGenerator from "./incomes_chart_generator"
 
-charGenerator()
+transfersChartGenerator()
+incomesChartGenerator()
 
 Rails.start()
 Turbolinks.start()
